35d56b1a19b49cf517e595a5494777a3ba95879c
2466402m <2466402m@student.gla.ac.uk>
2023-12-06 18:41:40 +0000

    firstunittest passed

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
-# Create your views here.
+def home_page(request):
+    return HttpResponse("<html><title>To-Do lists</title></html>")
