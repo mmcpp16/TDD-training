2d18d2123001d6e912a78ee5469a47eb05c34504
2466402m <2466402m@student.gla.ac.uk>
2023-12-07 16:48:25 +0000

    code smell: POST test is too long

--- lists/views.py
+++ lists/views.py
@@ -2,6 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
+    item = Item()
+    item.text = request.POST.get("item_text", "")
+    item.save()
+    
     return render(
         request,
         "home.html",
